f9038432cf231b63b13f78065f8ec1f4c6f61669
nt153133 <52430037+nt153133@users.noreply.github.com>
2019-10-24 17:37:53 -0400

    BattleCharacterExtension for targeting party member and prepare for boss check

---
+++ Helpers/BattleCharacterExtension.cs
@@ -0,0 +1,18 @@
+﻿using ff14bot.Enums;
+using ff14bot.Objects;
+
+namespace Deep.Helpers
+{
+    public static class BattleCharacterExtension
+    {
+        public static bool IsTargetingMyPartyMember(this BattleCharacter bc)
+        {
+            return bc.TargetCharacter != null && (bc.TargetCharacter.IsMe || (bc.TargetCharacter.StatusFlags & (StatusFlags.AllianceMember | StatusFlags.PartyMember)) != 0);
+        }
+        public static bool IsBoss(this BattleCharacter bc)
+        {
+            //return bc != null && BossManager.BossEncounters != null && BossManager.BossEncounters.Any(i => i.NpcId == bc.NpcId);
+            return false;
+        }
+    }
+}
