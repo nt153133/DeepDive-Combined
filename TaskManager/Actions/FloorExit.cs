0d510f39ea354232389e1b7a3f355b0b66a557c7
nt153133 <52430037+nt153133@users.noreply.github.com>
2019-10-24 17:29:20 -0400

    Switched to HoH exit logic, needs to be updated with new closer exit since Large rooms exist (Floors where instead of multiple rooms it's just one big room with multiple exits which include trap exits) Once merge is complete there needs to be a way to find out if floor is a big room or not...map5x probably

--- TaskManager/Actions/FloorExit.cs
+++ TaskManager/Actions/FloorExit.cs
@@ -21,14 +21,17 @@ using ff14bot.Pathing;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using ff14bot.Objects;
 
 namespace Deep.TaskManager.Actions
 {
-    class FloorExit : ITask
+    internal class FloorExit : ITask
     {
-        public string Name => "Floor Exit";
+        public static Vector3 location = Vector3.Zero;
+        private int Level;
 
         private Poi Target => Poi.Current;
+        public string Name => "Floor Exit";
 
         public async Task<bool> Run()
         {
@@ -44,22 +47,19 @@ namespace Deep.TaskManager.Actions
             {
                 await CommonTasks.MoveAndStop(new MoveToParameters(Target.Location, "Floor Exit"), 0.5f, true);
                 return true;
-            } else
-            {
-                await CommonTasks.StopMoving();
             }
-            
+
+            await CommonTasks.StopMoving();
+
             var _level = DeepDungeonManager.Level;
-            await Coroutine.Wait(-1, () => Core.Me.InCombat || _level != DeepDungeonManager.Level || CommonBehaviors.IsLoading || QuestLogManager.InCutscene);
+            await Coroutine.Wait(-1,
+                () => Core.Me.InCombat || _level != DeepDungeonManager.Level || CommonBehaviors.IsLoading ||
+                      QuestLogManager.InCutscene);
             Poi.Clear("Floor has changed or we have entered combat");
             Navigator.Clear();
             return true;
         }
 
-        private int Level = 0;
-        private Vector3 location = Vector3.Zero;
-
-        
 
         public void Tick()
         {
@@ -68,13 +68,13 @@ namespace Deep.TaskManager.Actions
 
             if (location == Vector3.Zero || Level != DeepDungeonManager.Level)
             {
-               
-                var ret = GameObjectManager.GetObjectByNPCId(EntityNames.OfPassage);
+                //var ret = GameObjectManager.GetObjectByNPCId(EntityNames.FloorExit);
+                GameObject ret = GameObjectManager.GameObjects.Where(r => r.NpcId == EntityNames.OfPassage)
+                    .OrderBy(r => r.Distance()).FirstOrDefault();
                 if (ret != null)
                 {
                     Level = DeepDungeonManager.Level;
                     location = ret.Location;
-                    
                 }
                 else if (Level != DeepDungeonManager.Level)
                 {
@@ -83,14 +83,25 @@ namespace Deep.TaskManager.Actions
                 }
             }
 
-            //if we are in combat don't move toward the carn of return
-            if (Poi.Current != null && (Poi.Current.Type == PoiType.Kill || Poi.Current.Type == PoiType.Wait || Poi.Current.Type == PoiType.Collect))
-                return;
-            
-            if (DDTargetingProvider.Instance.LevelComplete && !DeepDungeonManager.BossFloor && location != Vector3.Zero)
+            if (location != Vector3.Zero)
             {
-                Poi.Current = new Poi(location, PoiType.Wait);
+                GameObject ret = GameObjectManager.GameObjects.Where(r => r.NpcId == EntityNames.OfPassage)
+                    .OrderBy(r => r.Distance()).FirstOrDefault();
+                if (ret != null)
+                    if (Core.Me.Location.Distance2D(ret.Location) < location.Distance2D(ret.Location))
+                        location = ret.Location;
             }
+
+            //if we are in combat don't move toward the carn of return
+            if (Poi.Current != null && (Poi.Current.Type == PoiType.Kill || Poi.Current.Type == PoiType.Wait ||
+                                        Poi.Current.Type == PoiType.Collect))
+                return;
+
+            // if (location != Vector3.Zero && Navigator.NavigationProvider.LookupPathInfo(GameObjectManager.GameObjects.Where(r => r.NpcId == EntityNames.FloorExit).OrderBy(r => r.Distance(location)).FirstOrDefault(), 3).Navigability != PathNavigability.Navigable)
+            //     return;
+
+            if (DDTargetingProvider.Instance.LevelComplete && !DeepDungeonManager.BossFloor && location != Vector3.Zero)
+                Poi.Current = new Poi(location, PoiType.Wait);
         }
     }
 }
